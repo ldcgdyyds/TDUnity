4fea514cc93436950cc75e6114ec36fc855d2508
ldcgdyyds <1223655014@qq.com>
2025-08-04 20:52:33 +0800

    顺便提交

---
+++ Assets/Scripts/UI/SelectItem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
